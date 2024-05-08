8b30c8de6954497662e093461a1d58f4df9b631a
srdtfyguhijy6 <gs1passhand@hotmail.com>
2024-05-08 18:37:28 +0700

    update commit

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
-function bubbleSort(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = 0; j < arr.length - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
-    }
-  }
-  return arr;
-}
+const deepFlatten = (arr) =>
+  [].concat(...arr.map((v) => (Array.isArray(v) ? deepFlatten(v) : v)));
