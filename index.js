690d6e542599397b9e515f28083998dec55cc3b6
srdtfyguhijy6 <gs1passhand@hotmail.com>
2024-05-14 13:44:58 +0700

    update aptos project commit

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-function squareDigits(num) {
-  return parseInt(
-    num
-      .toString()
-      .split("")
-      .map((digit) => digit ** 2)
-      .join(""),
-  );
+function countAndSay(n) {
+  let result = "1";
+  for (let i = 1; i < n; i++) {
+    let temp = "";
+    let count = 1;
+    for (let j = 0; j < result.length; j++) {
+      if (result[j] === result[j + 1]) {
+        count++;
+      } else {
+        temp += count + result[j];
+        count = 1;
+      }
+    }
+    result = temp;
+  }
+  return result;
 }
